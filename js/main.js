81e4f0dbc2c3cac099946d1e70b284957e5a3aef
Lukas Vogel <lukasvogel@bluewin.ch>
2016-05-28 13:53:39 +0200

    Add Widget Controller Widgets and the Controller should be kept as separate as possible: A dynamic system that injects script files into the page's header will control the widgets.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@
 // (c) 2016 by Lukas Vogel
 //
 
-document.onload(function() {
+var plugin_list = ['widget-controller'];
+
+document.addEventListener('DOMContentLoaded', function() {
+    var head_frag = document.createDocumentFragment();
+    var node;
     
+    for (var i = 0; i < plugin_list.length; i++) {
+        node = document.createElement('script');
+        node.src = './js/' + plugin_list[i] + '.js';
+        head_frag.appendChild(node);
+    }
+    document.querySelector('head').appendChild(head_frag);
 });
---
+++ js/widget-controller.js
@@ -0,0 +1,14 @@
+//
+// widget-controller.js
+// Magic Mirror
+// (c) 2016 by Lukas Vogel
+//
+
+function WidgetController() {
+    var _initialized = false;
+    var that = this;            // workaround
+    
+    this.init = function() {
+        _initialized = true;
+    }
+}
