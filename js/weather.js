bb5ff383c6bdb3c65cba5d7403559013e51507cf
Lukas Vogel <lukasvogel@bluewin.ch>
2016-07-20 11:30:25 +0200

    Add weather data to the screen
    Now the current temperature is shown on the webpage, although it isn't really pretty yet.
    Sometimes the API freaks out and shows completely wrong values. I'm going to have to look into this.

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,10 @@
 // (c) 2016 by Lukas Vogel
 //
 
-function fetchWeatherData() {
+// Bad practise ahead
+var weather_lastFetchedData;
+
+function weather_fetchData(node) {
     var api_key = '424a922dab68fc46fbd86e31ae94846d'; // for easy future change
     var city_id = '2657970'; // Winterthur. Change later, maybe?
     var req = new XMLHttpRequest();
@@ -13,17 +16,30 @@ function fetchWeatherData() {
         /*
          * readyState == 4 -> data fully loaded
          * status == 200 -> HTTP: 200 OK
-        */
+         */
         if (req.readyState == 4 && req.status == 200) {
-            console.log(req.responseText);
+            // data successfully loaded, now parse to an object
+            var data = JSON.parse(req.responseText);
+
+            if (data == weather_lastFetchedData)
+                return;
+            else weather_lastFetchedData = data;
+
+            weather_updateDOM(node);
         }
         // error handling, maybe?
     };
 
     // My API key is 424a922dab68fc46fbd86e31ae94846d
-    req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + city_id + 
-        '&appid=' + api_key + '&units=metric&lang=de');
+    req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + 
+        city_id + '&appid=' + api_key + '&units=metric&lang=de');
     req.send();
 }
 
-new Widget('weather', fetchWeatherData, [9, 10, 11], 60000).register();
+function weather_updateDOM(node) {
+    node.innerHTML = 
+        '<div class="temp">' + weather_lastFetchedData.main.temp + '</div>' +
+        '<div class="cond">' + weather_lastFetchedData.weather[0].description + '</div>';
+}
+
+new Widget('weather', weather_fetchData, [9, 10, 11], 60000).register().applyStyles();
