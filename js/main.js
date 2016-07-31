3384392889dd80427295642ddf7ba09f14138bd4
Lukas Vogel <lukasvogel@bluewin.ch>
2016-07-31 18:02:45 +0200

    Add $() shortcut for DOM lookup
    Throughout the application it is probably easier to just write this shortcut once than to always write the more complicated document.querySelectorAll() etc.

--- js/main.js
+++ js/main.js
@@ -17,3 +17,19 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('head').appendChild(script);
 
 });
+
+/**
+ * This function is a shortcut to the document.querySelector[All] function in
+ * native EcmaScript. It provides a way of using jQuery's most popular feature,
+ * DOM node lookup, without actually requiring jQuery.
+ * @param {String[]} selector – The CSS like lookup selector.
+ */
+function $(selector) {
+    var result = document.querySelectorAll(selector);
+
+    // I don't want to write [0] all the time.
+    if (result.length > 1)
+        return result;
+    else
+        return result[0];
+}
