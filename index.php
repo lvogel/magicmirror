85b344be99f460e001bab915bf0b61954e971bfc
Lukas Vogel <lukasvogel@bluewin.ch>
2016-08-17 15:06:59 +0200

    Fix positioning issues with the weather widget and scrollbars.
    - Fixed unnecessary scrollbars because the body was larger than 100%.
    - Fixed the weather widget not respecting the widget controller's positioning. This was because the table cells were not set to position:relative, so the positioning context for child nodes inside the widget was still the entire body.
    See: Issue #3

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta charset="utf-8" />
         <title>Magic Mirror</title>
         <link rel="stylesheet" href="./css/main.css" />
+        <!-- In HTML and not CSS so it can be loaded asynchronously -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i" rel="stylesheet">
         <script src="./js/main.js"></script>
     </head>
