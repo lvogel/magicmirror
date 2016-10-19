18558518f79a4182b5deb7732daddb32decdf02b
Lukas Vogel <lukasvogel@bluewin.ch>
2016-10-19 18:17:03 +0200

    Add a function for AJAX requests
    Since the same method to make AJAX requests is used all over the application, the implementation of a standardized function reduces error and bug potential.

--- js/main.js
+++ js/main.js
@@ -42,3 +42,44 @@ function $(selector, context) {
     else
         return result[0];
 }
+
+/**
+ * This function replaces existing code in many different files by providing a
+ * framework that allows AJAX requests to files or URLs by using the GET
+ * protocol.
+ * @param {String} url - The URL that should be queried.
+ * @param {AsyncSuccessCallback} successCallback - The callback handling successful outcomes.
+ * @param {AsyncErrorCallback} errorCallback - The callback handling unsuccessful outcomes.
+ */
+function loadAsync(url, successCallback, errorCallback) {
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            // Download finished. Determine whether it was successful.
+            if (req.status == 200) {
+                successCallback(req.responseText);
+            } else {
+                errorCallback(req.status, req.responseText);
+            }
+        }
+    };
+
+    req.open('GET', url, true);
+    req.send();
+}
+
+ /**
+ * A callback that will be run as soon as the AJAX request has completed
+ * and if the HTTP status code equals to 200 (OK). 
+ * @callback AsyncSuccessCallback
+ * @param {String} responseText - The data that was retrieved from the URL.
+ */
+ 
+ /**
+  * A callback that will be run as soon as the AJAX request has completed
+  * and if an error occured (status code does not equal to 200 (OK)).
+  * @callback AsyncErrorCallback
+  * @param {Number} statusCode - The HTTP status code that the server returned.
+  * @param {String} responseText - The data that was retrieved from the URL.
+  */
